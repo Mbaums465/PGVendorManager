a30d95ecd62ad5b664cb3149603d39207bf9a610
Mbaums465 <142707320+Mbaums465@users.noreply.github.com>
2025-10-03 01:13:19 -0400

    Update VendorMe.bat

--- VendorMe.bat
+++ VendorMe.bat
@@ -2,8 +2,9 @@
 REM Navigate to the folder where the batch file is located
 cd /d "%~dp0"
 
-REM Run the Python script
-python PGVendorTracker.py
+REM Run the Python script. I actually call this the file 'PGVendorTracker.py' on my PC but the file isnt named that here. yolo. 
+python PGVendorManager.py
 
 REM Optional: pause to keep the window open
+
 pause
